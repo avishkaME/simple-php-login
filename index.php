93626e5a07f30eb235c4bcdd141877baf0d03847
avishkaME <ameddegoda143@gmail.com>
2019-05-10 10:42:56 +0530

    create basic html form

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>simple php login</title>
+  </head>
+  <body>
+    <h1>Login form</h1>
+    <form class="" action="index.html" method="post">
+      <input type="text" name="" value="" placeholder="username"><br>
+      <input type="password" name="" value="" placeholder="password"><br>
+      <input type="submit" name="btn" value="Login">
+    </form>
+  </body>
+</html>
